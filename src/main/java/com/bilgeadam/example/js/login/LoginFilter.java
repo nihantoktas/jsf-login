1831d3e38caf8437e4d08d74945c12a6f2bfd866
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-04-27 13:36:07 +0300

    ders26

---
+++ src/main/java/com/bilgeadam/example/js/login/LoginFilter.java
@@ -0,0 +1,38 @@
+package com.bilgeadam.example.js.login;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginFilter implements Filter{
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response= (HttpServletResponse) servletResponse;
+        HttpSession session=((HttpServletRequest) servletRequest).getSession(false);
+
+        String urlPath=request.getContextPath()+"/login.xhtml";
+
+        Boolean isSession=session!=null&&session.getAttribute("kullanıcı")!=null;
+        Boolean isLogin=request.getRequestURL().equals(urlPath);
+
+
+        if (isSession||isLogin){
+            filterChain.doFilter(request,response);
+        }else {
+            response.sendRedirect(urlPath);
+        }
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+}
